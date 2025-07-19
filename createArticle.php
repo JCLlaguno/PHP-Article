7466efe3327cef798a616b52d7bb9537c9d89f14
JCLlaguno <johnchristopherllaguno@gmail.com>
2025-07-19 15:21:52 +0800

    wip styled title content input fields and submit button on create article page

--- createArticle.php
+++ createArticle.php
@@ -3,7 +3,7 @@
     require_once './app/article.php';
     require_once './includes/header.php';
 
-    if(isset($_POST['submit'])){
+    if(isset($_POST['submit']) && !empty($_POST['title'] && !empty($_POST['content']))){
         $article_title = trim($_POST['title']);
         $article_content = trim($_POST['content']);
         $userid = $_SESSION['userid'];
@@ -15,11 +15,11 @@
 ?>
 <section class="create-article-section">
     <form class="create-article" action="" method="POST">
-        <label for="title">Title:</label><br>
-        <input type="text" id="title" name="title"><br>
-        <label for="content">Content</label><br>
-        <textarea name="content" id="content"></textarea>
-        <input type="submit" name="submit" value="Create Article">
+        <label for="title">Title:</label>
+        <textarea class="title" name="title" id="title"></textarea>
+        <label for="content">Content</label>
+        <textarea class="content" name="content" id="content"></textarea>
+        <input class="create-article-btn" type="submit" name="submit" value="Create Article">
     </form> 
 </section>
 <?php require_once('./includes/footer.php'); ?>
